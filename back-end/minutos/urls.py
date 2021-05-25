e1b0a7343b11802702d49644e33d0dcbbec2540a
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-25 11:44:54 -0700

    add privacy url

--- back-end/minutos/urls.py
+++ back-end/minutos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, privacy_page
 
 urlpatterns = [
-    path('',home_page)
+    path('',home_page),
+    path('/privacy',privacy_page, name="privacy"),
 ]
