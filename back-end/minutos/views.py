0767125430fd591b105c8c409031e8e08d40317c
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-26 08:50:03 -0700

    create views functions

--- back-end/minutos/views.py
+++ back-end/minutos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_page(request):
+    return render(request, 'home.html')
+
+def privacy_page(request):
+    return render(request, 'privacy.html')
