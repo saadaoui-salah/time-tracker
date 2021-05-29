808a21b1f1311cb3d1d2e24ccc3dc9f1899c2865
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-29 06:35:31 -0700

    add plans url

--- back-end/minutos/urls.py
+++ back-end/minutos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, privacy_page
+from .views import home_page, privacy_page, plans_page
 
 urlpatterns = [
     path('',home_page),
-    path('/privacy',privacy_page, name="privacy"),
+    path('/privacy', privacy_page, name="privacy"),
+    path('/plans', plans_page, name="plans"),
 ]
